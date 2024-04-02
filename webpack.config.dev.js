e276b0cbad81ba640e495c1d12830ef14bd9fbd8
Merve Sert <merveyildiz@demirorenmedya.com>
2024-04-02 15:33:03 +0300

    environment added

---
+++ webpack.config.dev.js
@@ -0,0 +1,44 @@
+const webpack = require("webpack");
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+process.env.NODE_ENV = "development";
+
+module.exports = {
+  mode: "development",
+  target: "web",
+  devtool: "cheap-module-source-map", // source mapp let us see our original code when debugging in the browser
+  entry: "./src/index",
+  output: {
+    path: path.resolve(__dirname, "build"),
+    publicPath: "/",
+    filename: "bundle.js"
+  },
+  devServer: {
+    stats: "minimal", // reduces information typed on the command line
+    overlay: true, // Shows a full-screen overlay in the browser when there are compiler errors or warnings.
+    historyApiFallback: true, // all request send to index.html
+    disableHostCheck: true,
+    headers: { "Access-Control-Allow-Origin": "*" },
+    https: false
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "src/index.html",
+      favicon: "src/favicon.ico"
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ["babel-loader", "eslint-loader"]
+      },
+      {
+        test: /(\.css)$/,
+        use: ["style-loader", "css-loader"]
+      }
+    ]
+  }
+};

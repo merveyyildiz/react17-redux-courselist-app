0a3fe1499e45222601c592c579e05113624324c0
Merve Sert <merveyildiz@demirorenmedya.com>
2024-05-07 16:25:29 +0300

    redux config completed

--- src/index.js
+++ src/index.js
@@ -3,13 +3,19 @@ import { render } from "react-dom";
 import { BrowserRouter as Router } from "react-router-dom";
 import App from "./components/App";
 import "bootstrap"; // import js file in bootstrap
+import { Provider } from "react-redux";
+import configureStore from "./redux/configureStore";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./styles/index.scss";
 
+const store = configureStore();
+
 render(
-  <Router>
-    <App />
-  </Router>,
+  <Provider store={store}>
+    <Router>
+      <App />
+    </Router>
+  </Provider>,
   document.getElementById("app")
 );
